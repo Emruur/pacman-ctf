535308107c59d15cd8db60452f702c00388d51b0
AndrewSpiro <77333606+AndrewSpiro@users.noreply.github.com>
2025-03-29 19:41:51 +0100

    using raveAgent.py instead of rave argument of myTeam

--- raveHeuristicTeam.py
+++ raveHeuristicTeam.py
@@ -1,6 +1,13 @@
-from myTeam import *
+# from myTeam import *
+
+# def createTeam(firstIndex, secondIndex, isRed,
+# 							 first = 'TreeSearch', second = 'TreeSearch'):
+
+# 	return [eval(first)(firstIndex, random_rolls = False, custom_heuristic = True, rave = True), eval(second)(secondIndex, random_rolls = False, custom_heuristic = True, rave = True)]
+
+from raveAgent import *
 
 def createTeam(firstIndex, secondIndex, isRed,
 							 first = 'TreeSearch', second = 'TreeSearch'):
 
-	return [eval(first)(firstIndex, random_rolls = False, custom_heuristic = True, rave = True), eval(second)(secondIndex, random_rolls = False, custom_heuristic = True, rave = True)]
+	return [eval(first)(firstIndex, custom_heuristic = True), eval(second)(secondIndex, custom_heuristic = True)]
--- raveTeam.py
+++ raveTeam.py
@@ -1,6 +1,7 @@
-from myTeam import *
+# from myTeam import *
 
-def createTeam(firstIndex, secondIndex, isRed,
-							 first = 'TreeSearch', second = 'TreeSearch'):
+# def createTeam(firstIndex, secondIndex, isRed,
+# 							 first = 'TreeSearch', second = 'TreeSearch'):
 
-	return [eval(first)(firstIndex, random_rolls = True, custom_heuristic = False, rave = True), eval(second)(secondIndex, random_rolls = True, custom_heuristic = False, rave = True)]
+# 	return [eval(first)(firstIndex, random_rolls = True, custom_heuristic = False, rave = True), eval(second)(secondIndex, random_rolls = True, custom_heuristic = False, rave = True)]
+from raveAgent import *
