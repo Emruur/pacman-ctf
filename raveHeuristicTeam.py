328b3818f2cf6dc0764aefb2ef644753ce9c2d53
AndrewSpiro <77333606+AndrewSpiro@users.noreply.github.com>
2025-03-29 19:54:30 +0100

    added comment about slowness

--- raveHeuristicTeam.py
+++ raveHeuristicTeam.py
@@ -4,7 +4,7 @@
 # 							 first = 'TreeSearch', second = 'TreeSearch'):
 
 # 	return [eval(first)(firstIndex, random_rolls = False, custom_heuristic = True, rave = True), eval(second)(secondIndex, random_rolls = False, custom_heuristic = True, rave = True)]
-
+#### if this is too slow, above is faster- but it uses the myTeam rave argument instead of raveAgent.py
 from raveAgent import *
 
 def createTeam(firstIndex, secondIndex, isRed,
