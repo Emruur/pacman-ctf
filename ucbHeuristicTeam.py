e29b966901dcc33a03d62af02d3bdc47c7746e4a
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2025-03-30 01:11:12 +0100

    ucb

--- ucbHeuristicTeam.py
+++ ucbHeuristicTeam.py
@@ -1,6 +1,8 @@
 from myTeam import *
 
+
 def createTeam(firstIndex, secondIndex, isRed,
 							 first = 'TreeSearch', second = 'TreeSearch'):
+
 
 	return [eval(first)(firstIndex, random_rolls = False, custom_heuristic = True, rave = False), eval(second)(secondIndex, random_rolls = False, custom_heuristic = True, rave = False)]
